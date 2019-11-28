c09f72d95d25bccd6615ce381bcb8e250623495c
Daniel Cenoz <cuervodaniel@rocketmail.com>
2019-11-28 08:55:54 -0300

    creada conexion con mongodb

--- rutas/rutas.js
+++ rutas/rutas.js
@@ -5,7 +5,6 @@ let contenidoIndex = controlador.Index;
 let rutas = function(app, puerto, controlador){
     
     app.get('/', (req, res) => {
-        console.log(contenidoIndex);
 
         res.render('index', contenidoIndex)
     });
@@ -22,6 +21,53 @@ let rutas = function(app, puerto, controlador){
 
     app.get('/blog', (req, res) => {
         res.render('blog');
+    });
+
+    app.post('/blog', (req, res) => {
+        console.log('recibido!');
+        let entradaString = JSON.stringify(req.body);
+        console.log(entradaString);
+        
+        //let db = require('../db/dbapp');
+        //let guarda = db.guardaArticulo;
+
+        let guardaArticulo = (entradaString) =>{
+            console.log('accediendo a guardaArticulo');
+            let mongoose = require('mongoose');
+          // make a connection
+          mongoose.connect('mongodb://localhost:27017/blog');
+          // get reference to database
+          let db = mongoose.connection;
+          db.on('error', console.error.bind(console, 'connection error:'));
+        
+        
+          let entrada = JSON.parse(entradaString);
+          db.once('open', function() {
+            console.log("Connection Successful!");    
+            // define Schema
+            let BlogSchema = mongoose.Schema({
+              autor: String,
+              fecha: Date,
+              post: String,
+              tags: String
+            });
+            // compile schema to model
+            let Articulo = mongoose.model('Articulo', BlogSchema, 'blog');
+            // a document instance
+            //recoger datos de body.request
+            let newArticulo = new Articulo({ autor: entrada.autor, fecha: entrada.fecha, post: entrada.post, tags: entrada.post});
+            // save model to database
+            newArticulo.save(function(err, newArticulo){
+              if(err) return console.error(err);
+              console.log(newArticulo + "guardado en BD")
+            });
+        
+          });
+          }
+
+        guardaArticulo(entradaString);
+        console.log('nueva entrada guardada!');
+        res.send(res.status);
     });
 
     app.get('/editor', (req, res) => {
